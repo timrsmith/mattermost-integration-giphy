a5e746e570e167f3e1b33929c3a86926df8d3465
THEBAULT Julien <julien@thebault.co>
2016-03-16 13:14:35 +0100

    The application is bulletproof

--- mattermost_giphy/app.py
+++ mattermost_giphy/app.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import logging
 import os
 import sys
 import json
@@ -13,6 +14,8 @@ from flask import Response
 from mattermost_giphy.settings import *
 
 
+logging.basicConfig(
+    level=logging.INFO, format='[%(asctime)s] [%(levelname)s] %(message)s')
 app = Flask(__name__)
 
 
@@ -30,65 +33,71 @@ def new_post():
     """
     Mattermost new post event handler
     """
+    try:
+        # NOTE: common stuff
+        slash_command = False
+        resp_data = {}
+        resp_data['username'] = USERNAME
+        resp_data['icon_url'] = ICON_URL
 
-    data = request.form
+        data = request.form
 
-    if MATTERMOST_GIPHY_TOKEN.find(data['token']) == -1:
-        print('Tokens did not match, it is possible that this request came from somewhere other than Mattermost')
-        return 'OK'
+        if not 'token' in data:
+            raise Exception('Missing necessary token in the post data')
 
-    # NOTE: support the slash command
-    slash_command = False
-    if 'command' in data:
-        slash_command = True
+        if MATTERMOST_GIPHY_TOKEN.find(data['token']) == -1:
+            raise Exception('Tokens did not match, it is possible that this request came from somewhere other than Mattermost')
 
-    translate_text = data['text']
-    if not slash_command:
-        translate_text = data['text'][len(data['trigger_word']):]
+        # NOTE: support the slash command
+        if 'command' in data:
+            slash_command = True
+            resp_data['response_type'] = 'in_channel'
 
-    if len(translate_text) == 0:
-        print("No translate text provided, not hitting Giphy")
-        return 'OK'
+        translate_text = data['text']
+        if not slash_command:
+            translate_text = data['text'][len(data['trigger_word']):]
 
-    gif_url = giphy_translate(translate_text)
+        if not translate_text:
+            raise Exception("No translate text provided, not hitting Giphy")
 
-    if len(gif_url) == 0:
-        print('No gif url found, not returning a post to Mattermost')
-        return 'OK'
+        gif_url = giphy_translate(translate_text)
+        if not gif_url:
+            raise Exception('No gif url found, not returning a post to Mattermost')
 
-    resp_data = {}
-    resp_data['text'] = '''`{}` searched for {}
-{}'''.format(data.get('user_name', 'unknown').title(), translate_text, gif_url)
-    resp_data['username'] = USERNAME
-    resp_data['icon_url'] = ICON_URL
-    if slash_command:
-        resp_data['response_type'] = 'in_channel'
-
-    resp = Response(content_type='application/json')
-    resp.set_data(json.dumps(resp_data))
-
-    return resp
+        resp_data['text'] = '''`{}` searched for {}
+    {}'''.format(data.get('user_name', 'unknown').title(), translate_text, gif_url)
+    except Exception as err:
+        msg = '{} {}'.format(err, err.message)
+        logging.error('unable to handle new post :: {}'.format(msg))
+        resp_data['text'] = msg
+    finally:
+        resp = Response(content_type='application/json')
+        resp.set_data(json.dumps(resp_data))
+        return resp
 
 
 def giphy_translate(text):
     """
     Giphy translate method, uses the Giphy API to find an appropriate gif url
     """
+    try:
+        params = {}
+        params['s'] = text
+        params['rating'] = RATING
+        params['api_key'] = GIPHY_API_KEY
 
-    params = {}
-    params['s'] = text
-    params['rating'] = RATING
-    params['api_key'] = GIPHY_API_KEY
+        resp = requests.get('https://api.giphy.com/v1/gifs/translate', params=params, verify=True)
 
-    resp = requests.get('https://api.giphy.com/v1/gifs/translate', params=params, verify=True)
+        if resp.status_code is not requests.codes.ok:
+            logging.error('Encountered error using Giphy API, text=%s, status=%d, response_body=%s' % (text, resp.status_code, resp.json()))
+            return None
 
-    if resp.status_code is not requests.codes.ok:
-        print('Encountered error using Giphy API, text=%s, status=%d, response_body=%s' % (text, resp.status_code, resp.json()))
-        return ''
+        resp_data = resp.json()
 
-    resp_data = resp.json()
+        url = list(urlsplit(resp_data['data']['images']['original']['url']))
+        url[0] = SCHEME.lower()
 
-    url = list(urlsplit(resp_data['data']['images']['original']['url']))
-    url[0] = SCHEME.lower()
-
-    return urlunsplit(url)
+        return urlunsplit(url)
+    except Exception as err:
+        logging.error('unable to translate giphy :: {}'.format(err))
+        return None
